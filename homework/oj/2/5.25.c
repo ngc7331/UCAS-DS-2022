2a0a63f012c2c478ebfe23fdeac9cbae19f9a0d7
ngc7331 <ngc7331@outlook.com>
2022-04-25 09:18:56 +0800

    feat: 5.25

--- homework/oj/2/5.25.c
+++ homework/oj/2/5.25.c
@@ -3,41 +3,105 @@
 
 #define CACHESIZE 10000
 
+typedef int ElemType;
+
+typedef struct __node {
+    struct __node *next;
+    ElemType data;
+} LNode, *LList;
+
+int InitList(LList *head) {
+    *head = (LList) malloc(sizeof(LNode));
+    (*head)->data = 0;
+    (*head)->next = NULL;
+}
+
+int InsertList(LList head, ElemType data) {
+    LNode *n = (LNode *) malloc(sizeof(LNode));
+    n->next = head->next;
+    n->data = data;
+    head->data ++;
+    head->next = n;
+    return 0;
+}
+
+void InputList(char *input, LList head) {
+    int x, offset;
+    if (sscanf(input, "%d%n", &x, &offset) != 1)
+        return ;
+    InputList(input+offset, head);
+    InsertList(head, x);
+}
+
+void PrintList(LList head) {
+    LNode *p = head->next;
+    if (p==NULL) printf("\n");
+    while (p != NULL) {
+        printf("%d%c", p->data, p->next==NULL?'\n':' ');
+        p = p->next;
+    }
+}
+
 // T(m, n) = O(m * n)
 
 int main() {
     int m, n;
     int x, offset;
-    char B1[10000][10000], B2;
-    char *tmp = malloc(CACHESIZE+1);
+    LList V1, V2;
+    InitList(&V1); InitList(&V2);
+    int B1[1000][1000], B2;
+    char tmp[CACHESIZE];
     scanf("%d %d", &m, &n);
-    // get A1
+    // get V1
+    getchar();
     fgets(tmp, CACHESIZE, stdin);
-    while (sscanf(tmp, "%d%n", &x, &offset) == 1) {
-        tmp += offset;
-        // TODO
-    }
+    InputList(tmp, V1);
     // get B1
     for (int i=0; i<m; i++)
         for (int j=0; j<n; j++)
-            scanf("%d", B1[i][j]);
-    // get A2
+            scanf("%d", &B1[i][j]);
+    // get V2
+    getchar();
     fgets(tmp, CACHESIZE, stdin);
-    while (sscanf(tmp, "%d%n", &x, &offset) == 1) {
-        tmp += offset;
-        // TODO
-    }
+    InputList(tmp, V2);
     // get B2 & caculate
+    LNode *p1=V1->next, *pp1=V1, *p2=V2->next;
     for (int i=0; i<m; i++) {
         for (int j=0; j<n; j++) {
-            scanf("%d", B2);
+            scanf("%d", &B2);
             if (B2 & B1[i][j]) {
-                // 均非零
+                // B1，B2均非零
+                if (p1->data + p2->data == 0) {
+                    B1[i][j] = 0;
+                    pp1->next = p1->next;
+                }
+                else {
+                    p1->data = p1->data + p2->data;
+                    pp1 = pp1->next;
+                }
+                p1 = p1->next;
+                p2 = p2->next;
             }
-            else if (B2 | B1[i][j]) {
-                // 仅一个矩阵非零
+            else if (B1[i][j]) {
+                // 仅B1非零
+                p1 = p1->next;
+                pp1 = pp1->next;
+            }
+            else if (B2) {
+                // 仅B2非零
+                pp1->next = p2;
+                pp1 = p2;
+                p2 = p2->next;
+                pp1->next = p1;
+                B1[i][j] = 1;
             }
         }
     }
+    // print V
+    PrintList(V1);
+    // print B
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            printf("%d%c", B1[i][j], j==n-1?'\n':' ');
     return 0;
 }
