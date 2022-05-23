ff44e8b57eb3ab18c18f17c32de49cdf7846a1bf
ngc7331 <ngc7331@outlook.com>
2022-05-24 00:27:29 +0800

    docs: update info from oj

--- homework/oj/1/2.41.c
+++ homework/oj/1/2.41.c
@@ -1,3 +1,41 @@
+/*
+# 2.41-稀疏多项式求导
+## 描述
+试以循环链表作稀疏多项式的存储结构，编写求其导函数的算法，要求利用原多项式中的节点空间存放其导函数（多项式），同时释放所有无用（被删）节点。
+稀疏多项式中采用的循环链表存储结构**LinkedPoly**定义为
+'''
+typedef struct PolyNode{
+  PolyTerm data;
+  Struct PolyNode *next;
+} PolyNode, *PolyLink;
+
+typedef PolyLink LinkedPoly;
+
+'''
+
+## 输入
+输入为给定的多项式
+
+## 输出
+输出为求导之后的结果，按照多项式中每一项的幂次由高到低排列
+如果导数为0，则输出0
+
+## 样例1
+### 输入
+- 2x + 4x^2 + 2x^3 + 3x^3 + 2x + 3x^2 - 3x^10
+
+### 输出
+- 30x^9 + 15x^2 + 14x
+
+## 样例2
+### 输入
+3x^2 + 4
+
+### 输出
+6x
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
--- homework/oj/2/4.c
+++ homework/oj/2/4.c
@@ -1,3 +1,181 @@
+/*
+# 4-去除C程序中的注释
+## 描述
+C程序的注释用\/\\*...\\*\/来表示。请写一个程序，将输入的C程序源代码中的注释去掉，输出去掉注释之后的源代码。
+注意：
+[1] 用于测试的C代码保证符合语法，不使用C++的//注释语法。
+[2] C语言不允许出现嵌套注释。具体来说，对于\/\\*\/\\*\\*\/"\\*\/"，如果不允许嵌套注释，那么它表示字符串"\\*\/"；如果允许嵌套注释，它表示一个引号"。
+[3] 字符串中出现的注释符\/\\*属于字符串的一部分，注释中出现的双引号"属于注释的一部分。
+
+## 输入
+符合语法的C代码文本文件。代码每行不超过200个字符。
+
+## 输出
+去掉注释后的C代码。要求只能去掉注释，不可以做其他的修改，比如调整缩进，去除注释之外的换行符等。
+
+## 样例1
+### 输入
+\/\\*Hash Search: 
+Hash function: division method; 
+handling collisions: open addressing's linear probing. 
+In this exercise, M is the basic area's length, all keys are non negative integers.\\*\/
+
+#define M 11
+
+int hash(int key)
+{
+	return key % M;
+}
+
+void init_hash(int* hashtable)
+{
+	int i;
+	for(i = 0; i < M; ++i)
+	{
+		hashtable[i] = -1;
+	}
+}
+
+\/\\*return value: 
+1:found, *position is the key's index; 
+0:not found, *position is where to insert the key; 
+-1:overflow. \\*\/
+int search_hash(int* hashtable, int key, int* position)
+{
+	int i, h = hash(key);
+	for(i = 0; i < M; ++i)
+	{
+		if(key == hashtable[h])
+		{
+			*position = h;
+			return 1;
+		}
+		if(-1 == hashtable[h])
+		{
+			*position = h;
+			return 0;
+		}
+		h = (h+1) % M;
+	}
+	*position = -1;
+	return -1;
+}
+
+\/\\*return value: 1:inserted, 0:overflow\\*\/
+int insert_hash(int* hashtable, int key)
+{
+	int position, result;
+	result = search_hash(hashtable, key, &position);
+	if(-1 == result)
+		return 0;
+	hashtable[position] = key;
+	return 1;
+}
+
+void main()
+{
+	int hashtable[M];
+	init_hash(hashtable);
+	srand(time(NULL));
+	int i, j, key;
+	for(i = 0; i < 8; ++i) 	\/\\*make a hash table with 8 elements\\*\/
+	{
+		key = rand() % 50;
+		insert_hash(hashtable, key);
+		printf("Insert %d\n", key);
+		for(j = 0; j < M; ++j)
+			printf("%3d", hashtable[j]);
+		printf("\n");
+	}
+
+	printf("Please input the key to search:\n");
+	scanf("%d", &key);
+	i = search_hash(hashtable, key, &j);
+	if(1 == i)
+		printf("Found!Its index is %d\n", j);
+	else
+		printf("Not found!\n");
+}
+
+### 输出
+
+
+#define M 11
+
+int hash(int key)
+{
+	return key % M;
+}
+
+void init_hash(int* hashtable)
+{
+	int i;
+	for(i = 0; i < M; ++i)
+	{
+		hashtable[i] = -1;
+	}
+}
+
+
+int search_hash(int* hashtable, int key, int* position)
+{
+	int i, h = hash(key);
+	for(i = 0; i < M; ++i)
+	{
+		if(key == hashtable[h])
+		{
+			*position = h;
+			return 1;
+		}
+		if(-1 == hashtable[h])
+		{
+			*position = h;
+			return 0;
+		}
+		h = (h+1) % M;
+	}
+	*position = -1;
+	return -1;
+}
+
+
+int insert_hash(int* hashtable, int key)
+{
+	int position, result;
+	result = search_hash(hashtable, key, &position);
+	if(-1 == result)
+		return 0;
+	hashtable[position] = key;
+	return 1;
+}
+
+void main()
+{
+	int hashtable[M];
+	init_hash(hashtable);
+	srand(time(NULL));
+	int i, j, key;
+	for(i = 0; i < 8; ++i) 	
+	{
+		key = rand() % 50;
+		insert_hash(hashtable, key);
+		printf("Insert %d\n", key);
+		for(j = 0; j < M; ++j)
+			printf("%3d", hashtable[j]);
+		printf("\n");
+	}
+
+	printf("Please input the key to search:\n");
+	scanf("%d", &key);
+	i = search_hash(hashtable, key, &j);
+	if(1 == i)
+		printf("Found!Its index is %d\n", j);
+	else
+		printf("Not found!\n");
+}
+
+*/
+
 #include <stdio.h>
 
 typedef enum {
