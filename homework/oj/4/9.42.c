b329ba4da3e270b637b160c60957d349d97438cd
ngc7331 <ngc7331@outlook.com>
2022-06-27 12:22:40 +0800

    fix: get oj info

--- homework/oj/4/9.42.c
+++ homework/oj/4/9.42.c
@@ -1,3 +1,47 @@
+/*
+# 9.42-9.43 Trie 树插入删除和查找
+## 描述
+**假设Trie树上同义词放在同一叶子结点中，请实现 Trie 树的插入、删除、查找功能**
+
+## 输入
+输入为若干行，每行包括一个数字和一个字符串(只包含小写字母)，数字 1、2、3 分别表示插入、删除、查找。如果删除的元素不存在，则不作任何操作。查找的元素存在输出为 True, 不存在输出为 False.
+
+## 输出
+输出的行数为查找操作的次数，若查找的元素存在输出为 True, 不存在输出为 False。
+
+## 样例1
+### 输入
+1 apple
+1 app
+1 at
+2 app
+3 apple
+2 apple
+3 apple
+
+### 输出
+True
+False
+
+## 样例2
+### 输入
+1 a
+1 b
+1 c
+1 ab
+1 acd
+2 ac
+3 ac
+3 abc
+3 ab
+
+### 输出
+False
+False
+True
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
